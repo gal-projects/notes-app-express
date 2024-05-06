e2eeefbb9d0a448628e02dd51e00b8b6f5660630
Galina Atamankina <g.atamankina@gmail.com>
2024-05-06 10:55:58 +0200

    Adding cors.

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import { defaultRouter } from './routes/default';
 import { infoRouter } from './routes/info'
 import { notesRouter } from './routes/notes'
-
+import cors from 'cors'
 
 // Create the express app
 const app = express();
 
 // Setup custom middleware
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
